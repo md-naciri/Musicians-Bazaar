6041dc2e723def946d0a60748afba3386996a975
md_naciri <med.ar.naciri@gmail.com>
2023-03-06 17:56:49 +0100

    Adding picture column to the user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('pic')->nullable();
+            $table->string('fb_id')->nullable();
+            $table->string('adress')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
