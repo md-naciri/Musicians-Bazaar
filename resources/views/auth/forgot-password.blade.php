56e390e2556c416363629d6cf32badefa11a7624
md_naciri <med.ar.naciri@gmail.com>
2023-03-06 19:03:20 +0100

    Forgot-password and Reset-password - Done

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="text-center mt-2">
-                        <h4>Login</h4>
+                        <h6>Enter your email</h6>
                         @if(session('status'))
                         <div class="alert alert-success">
                             {{session('status')}}
@@ -14,7 +14,8 @@
                         @endif
                     </div>
                     <div class="card-body">
-                        <form action="" method="post">@csrf
+                        <form action="{{route('password.request')}}" method="post">@csrf
+
                             <div class="form-group row mb-2">
                                 <label for="email" class="col-md-4 col-form-label text-md-right">
                                     Email
@@ -32,46 +33,15 @@
                                     @enderror
                                 </div>
                             </div>
-                            <div class="form-group row mb-2">
-                                <label for="password" class="col-md-4 col-form-label text-md-right">
-                                    Password
-                                </label>
-                                <div class="col-md-6">
-                                    <input type="password" name="password"
-                                        class="form-control @error('password') is-invalid @enderror">
-                                    @error('password')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>
-                                                {{ $message }}
-                                            </strong>
-
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="form-group row mb-2">
-
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" name="remember" id="remember"
-                                            {{ old('remember') ? 'checked' : '' }}>
-                                        <label class="form-check-label" for="label">Remember me</label>
-                                    <p><a href="{{route('password.request')}}">Forgotten a password?</a></p>
-                                    </div>
-                                </div>
-                            </div>
                             <div class="form-group row mb-0">
                                 
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-danger">Login</button>
+                                    <button type="submit" class="btn btn-danger">Link</button>
 
                                 </div>
                             </div>
                             
                             
-                           
-
-
                         </form>
                     </div>
                 </div>
