4a19ef015b7636b93845fc95f1880ba066809432
md_naciri <med.ar.naciri@gmail.com>
2023-03-10 19:29:06 +0100

    Seeding countries, states and cities
    Getting seeding data from a github repository, here is the link to it: https://github.com/mshoaibdev/laravel-country-states-city-seeds-migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(CountryTableSeeder::class);
+        $this->call(StateTableSeeder::class);
+        $this->call(CityTableSeeder::class);
     }
 }
