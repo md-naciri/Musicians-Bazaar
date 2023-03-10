fe219fcda5f5019a3b7e24e166aeae82739c0f3a
md_naciri <med.ar.naciri@gmail.com>
2023-03-10 20:06:05 +0100

    Migrating the instruments table
    The insruments table is the table where there is all the info about the product that can a user publish

---
+++ database/migrations/2023_03_10_184001_create_instruments_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instruments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('title');
+            $table->string('image1');
+            $table->string('image2');
+            $table->string('image3');
+            $table->integer('category_id');
+            $table->integer('subcategory_id')->nullable();
+            $table->text('text');
+            $table->string('price');
+            $table->string('inst_condition');
+            $table->string('adress')->nullable();
+            $table->integer('country_id');
+            $table->integer('state_id')->nullable();
+            $table->integer('city_id')->nullable();
+            $table->string('phone');
+            $table->integer('article_status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instruments');
+    }
+};
