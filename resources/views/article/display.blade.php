9afdb5afaedfe3946a68ef2609792aa89e84a7e7
md_naciri <med.ar.naciri@gmail.com>
2023-03-24 01:54:44 +0000

    Adding disqus for comments

--- resources/views/article/display.blade.php
+++ resources/views/article/display.blade.php
@@ -91,6 +91,28 @@
                 <a class="text-dark" href="{{route('cat', $article->category->slug)}}">Browse for more
                     {{$article->category->name}}</a>
             </p>
+            <hr>
+            <div class="border border-dark-subtle rounded p-2" id="disqus_thread" style="background-color: #f3f3f3">
+            </div>
+            <script>
+                /**
+                *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+                /*
+                var disqus_config = function () {
+                this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                };
+                */
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://musimart.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered
+                    by Disqus.</a></noscript>
         </div>
     </div>
 
